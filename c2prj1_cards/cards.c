c6f5c3c6bf8392903ab6444e37894e40b04d1fef
ginolee <ginolee22@outlook.com>
2019-02-03 12:21:42 -0700

    Start testing for c2proj1_cards

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,34 +5,148 @@
 
 
 void assert_card_valid(card_t c) {
-
-
+  /*
+  * use assert() to check that the card passed in has valid values
+  * value: 2 <= value <= VALUE_ACE
+  * suit: SPADES <= suit <= CLUBS
+  */
+  assert(c.value >= 2 && c.value <= VALUE_ACE);
+  assert(c.suit >= SPADES && c.suit <= CLUBS);
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+  // return "";
+  /*
+  * convert the hand_ranking_t enumerated value passed in to a string
+  */
+  switch(r) {
+    case STRAIGHT_FLUSH:
+      return "STRAIGHT_FLUSH";
+    case FOUR_OF_A_KIND:
+      return "FOUR_OF_A_KIND";
+    case FULL_HOUSE:
+      return "FULL_HOUSE";
+    case FLUSH:
+      return "FLUSH";
+    case STRAIGHT:
+      return "STRAIGHT";
+    case THREE_OF_A_KIND:
+      return "THREE_OF_A_KIND";
+    case TWO_PAIR:
+      return "TWO_PAIR";
+    case PAIR:
+      return "PAIR";
+    default:
+      return "NOTHING";
+  }
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  // return 'x';
+  /*
+  * return the character that textually represents the value of the passed-in card
+  */
+  char base = '0';
+  switch(c.value) {
+    case 10:
+      return base;
+    case VALUE_JACK:
+      return 'J';
+    case VALUE_QUEEN:
+    return 'Q';
+    case VALUE_KING:
+      return 'K';
+    case VALUE_ACE:
+      return 'A';
+    default:
+      return base + c.value;
+  }
 }
 
 
 char suit_letter(card_t c) {
-  return 'x';
-  
+  // return 'x';
+  /*
+  * return the letter that textually represents the suit of the card passed in
+  */
+  switch(c.suit) {
+    case SPADES:
+      return 's';
+    case HEARTS:
+      return 'h';
+    case DIAMONDS:
+      return 'd';
+    case CLUBS:
+      return 'c';
+    default:
+      return 'N';   // not valid
+  }
 }
 
 void print_card(card_t c) {
-
+  /*
+  * print out the textual representation of the card
+  */
+  printf("%c%c", value_letter(c), suit_letter(c));
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
-  card_t temp;
-  return temp;
+  // card_t temp;
+  // return temp;
+  /*
+  * make and return a card_t whose value and suit correspond to the letters passed in
+  */
+  card_t temp_card;
+  switch(value_let) {
+    case 'A':
+      temp_card.value = VALUE_ACE;
+      break;
+    case 'K':
+      temp_card.value = VALUE_KING;
+      break;
+    case 'Q':
+      temp_card.value = VALUE_QUEEN;
+      break;
+    case 'J':
+      temp_card.value = VALUE_JACK;
+      break;
+    case '0':
+      temp_card.value = 10;
+      break;
+    default:
+      temp_card.value = value_let - '0';
+  }
+  switch(suit_let) {
+    case 's':
+      temp_card.suit = SPADES;
+      break;
+    case 'h':
+      temp_card.suit = HEARTS;
+      break;
+    case 'd':
+      temp_card.suit = DIAMONDS;
+      break;
+    case 'c':
+      temp_card.suit = CLUBS;
+      break;
+    default:
+      temp_card.suit = 100;
+      printf("Should not enter here!");
+  }
+  // check
+  assert_card_valid(temp_card);
+  return temp_card;
 }
 
 card_t card_from_num(unsigned c) {
+  // card_t temp;
+  // return temp;
+  /*
+  * take a number from 0 (inclusive) to 52 (exclusive) and map it uniquely to
+  * a card value/suit combination.
+  */
   card_t temp;
+  temp.value = c % 13 + 2;
+  temp.suit = c / 13;
   return temp;
 }
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,37 @@
+#include <stdio.h>
+#include <assert.h>
 #include "cards.h"
 
 int main(void) {
+  // valid card test
+  printf("***** Valid Card Test *****\n");
+  card_t valid_card_test;
+  valid_card_test.value = VALUE_ACE;
+  valid_card_test.suit = SPADES;
+  printf("----- test of assert_card_valid() -----\n");
+  assert_card_valid(valid_card_test);
+  printf("----- assert_card_valid() passed -----\n");
 
+  printf("----- test of value_letter() & suit_letter() -----\n");
+  assert(value_letter(valid_card_test) == 'A');
+  assert(suit_letter(valid_card_test) == 's');
+  printf("----- value_letter() & suit_letter() passed -----\n");
+
+  printf("----- test of card_from_letters() & print_card() -----\n");
+  card_t new_card_from_letter = card_from_letters('A', 's');
+  print_card(new_card_from_letter);
+  printf("The result should be As\n");
+
+  printf("----- test of card_from_num() -----\n");
+  card_t new_card_from_number = card_from_num(12);
+  print_card(new_card_from_number);
+  printf("----- card_from_num() passed -----\n");
+
+  // invalid card test
+  printf("\n***** Invalid Card Test *****\n");
+  card_t invalid_card_test;
+  invalid_card_test.value = 100;
+  invalid_card_test.suit = 10;
+  printf("----- test of assert_card_valid() -----\n");
+  assert_card_valid(invalid_card_test);
 }
