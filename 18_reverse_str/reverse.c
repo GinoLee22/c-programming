600d625a762474bdce6e1be89b9b9d194e048e48
ginolee <ginolee22@outlook.com>
2019-02-06 18:10:55 -0700

    Did assignment 18

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -3,7 +3,23 @@
 #include <string.h>
 
 void reverse(char * str) {
-  //WRITE ME!
+  // 1. find the length of str
+  int length = 0;
+  char *p = str;
+  while (*p != '\0') {
+    length++;
+    p++;
+  }
+  // printf("length is: %d\n", length);
+  // 2. swap
+  char *p1 = str, *p2 = str + length - 1;
+  while (p1 < p2) {
+    char temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+    p1++;
+    p2--;
+  }
 }
 
 int main(void) {
