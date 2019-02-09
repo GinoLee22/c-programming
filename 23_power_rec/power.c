d971585c187d62fa59e6ae425555c1e7066d4276
Coursera Learner <nobody@nowhere.nul>
2019-02-09 03:06:37 +0000

    Did assignment 23

--- 23_power_rec/power.c
+++ 23_power_rec/power.c
@@ -3,17 +3,19 @@
 
 unsigned power(unsigned x, unsigned y);
 
+/*
 int main(void) {
   return EXIT_SUCCESS;
 }
+*/
 
 unsigned power(unsigned x, unsigned y) {
   // special case
   if (x == 0 && y == 0) {
-    return 0;
+    return 1;
   }
   if (x == 0) {
-    return 1;
+    return 0;
   }
   // base case
   if (y == 0) {
