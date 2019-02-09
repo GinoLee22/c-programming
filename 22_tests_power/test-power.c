829917ba6b1743230363d23dd346f424fbdb92e3
Coursera Learner <nobody@nowhere.nul>
2019-02-09 03:07:33 +0000

    Change Assignment test-power.c for testing clearance

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 unsigned power (unsigned x, unsigned y);
-void run_check(unsigned x, unsigned y, unsigned expected_ans);
+void run_check(unsigned x, unsigned y, unsigned expected_ans, int case_number);
 
 int main(void) {
   /* test:
@@ -13,25 +13,25 @@ int main(void) {
     2 ^ 3 -> 8
     -1 ^ 2 -> 1
   */
-  run_check(0, 0, 1);
-  run_check(0, 1, 0);
-  run_check(0, 2, 0);
-  run_check(1, 0, 1);
-  run_check(2, 0, 1);
-  run_check(1, 1, 1);
-  run_check(1, 2, 1);
-  run_check(2, 3, 8);
-  run_check(2, 30, 1073741824);
-  run_check(-1, 2, 1);
+  run_check(0, 0, 1, 1);
+  run_check(0, 1, 0, 2);
+  run_check(0, 2, 0, 3);
+  run_check(1, 0, 1, 4);
+  run_check(2, 0, 1, 5);
+  run_check(1, 1, 1, 6);
+  run_check(1, 2, 1, 7);
+  run_check(2, 3, 8, 8);
+  run_check(2, 30, 1073741824, 9);
+  run_check(-1, 2, 1, 10);
   return EXIT_SUCCESS;
 }
 
-void run_check(unsigned x, unsigned y, unsigned expected_ans) {
+void run_check(unsigned x, unsigned y, unsigned expected_ans, int case_number) {
   unsigned test_ans = power(x, y);
   if (test_ans == expected_ans) {
     printf("Correct!\n");
   } else {
-    printf("Wrong!\n");
+    printf("Wrong! Fail case number: %d\n", case_number);
     exit(EXIT_FAILURE);
   }
 }
