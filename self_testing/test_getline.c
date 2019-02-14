433611ebd94e37617e4d34ed7219e3508fbf0777
ginolee <ginolee22@outlook.com>
2019-02-13 20:59:12 -0800

    Test with valgrind

---
+++ self_testing/test_getline.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+  size_t sz = 0 ;
+  char * line = NULL;
+  getline(&line, &sz, stdin);
+  printf("%d\n", (int)sz);
+  // free(line);          // comment OR uncomment this line for Valgrind test
+}
